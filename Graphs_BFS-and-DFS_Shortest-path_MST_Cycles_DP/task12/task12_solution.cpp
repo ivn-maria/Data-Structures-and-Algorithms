c2c69a5d709efb2c783413be5ec698223c94af4b
Maria I <96080313+ivn-maria@users.noreply.github.com>
2023-09-03 12:44:25 +0300

    Update task12_solution.cpp

--- Graphs_BFS-and-DFS_Shortest-path_MST_Cycles_DP/task12/task12_solution.cpp
+++ Graphs_BFS-and-DFS_Shortest-path_MST_Cycles_DP/task12/task12_solution.cpp
@@ -1,65 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <climits>
 using namespace std;
 
-bool condition(const int count[]) {
-    for (int i = 0; i <= 25; i++){
-        if (count[i] != 0 && count[i] % 2 == 0) {
+unordered_map<char, int> words;
+
+bool isPossible(const unordered_map<char, int> &tempWords) {
+    for (const auto &element: tempWords) {
+        if (!(element.second & 1)) {
             return false;
         }
     }
+
     return true;
 }
 
-int minPartion(string& str) {
-    int n = str.size();
+int main() {
+    string word;
+    cin >> word;
 
-    int** even = new int*[n];
-    for (int i = 0; i < n; ++i)
-        even[i] = new int[n];
+    vector<int> subArrays(word.size() + 1, INT_MAX);
+    subArrays[0] = 0;
 
-    int** minSplits = new int*[n];
-    for (int i = 0; i < n; ++i)
-        minSplits[i] = new int[n];
-
-    for (int i = 0; i < n; i++) {
-        even[i][i] = true;
-        minSplits[i][i] = 0;
-    }
-
-
-    for (int i = 0; i < n; i++) {
-        int count2[26] = {0};
-        for (int j = i; j < n; j++) {
-            count2[str[j] - 'a']++;
-            even[i][j] = condition(count2);
-        }
-    }
-
-
-
-    for (int l = 2; l <= n; l++) {
-        for (int i = 0; i < n - l + 1; i++) {
-            int j = i + l - 1;
-
-            if (even[i][j]) minSplits[i][j] = 0;
-            else {
-                minSplits[i][j] = INT_MAX;
-                for (int k = i; k <= j - 1; k++)
-                    minSplits[i][j] = min(minSplits[i][j], minSplits[i][k] + minSplits[k + 1][j] + 1);
+    for (int i = 0; i < word.size() - 1; ++i) {
+        if (subArrays[i] != INT_MAX) {
+            words.clear();
+            for (int j = i; j < word.size(); ++j) {
+                words[word[j]]++;
+                if (isPossible(words)) {
+                    subArrays[j + 1] = min(subArrays[i] + 1, subArrays[j + 1]);
+                }
             }
         }
     }
 
-    return minSplits[0][n - 1] + 1;
+    cout << subArrays[word.size()];
 }
-
-int main() {
-    string s;
-    cin >> s;
-
-    cout << minPartion(s);
-
-    return 0;
-}
-
