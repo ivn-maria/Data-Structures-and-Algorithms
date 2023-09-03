521c7152cb2bb15ed106356fefb789fa6318901f
Maria I <96080313+ivn-maria@users.noreply.github.com>
2023-09-03 11:35:36 +0300

    Update task12_solution.cpp

--- Counting-and-Sorting-Algorithms_List_Stack_Queue/task12/task12_solution.cpp
+++ Counting-and-Sorting-Algorithms_List_Stack_Queue/task12/task12_solution.cpp
@@ -1,174 +1,190 @@
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-struct Node {
-    int value;
-    Node *previous;
-    Node *next;
+struct Node
+{
+    int color = 0;
+    int number = 0;
+    Node* previous = nullptr;
+    Node* next = nullptr;
 
-    Node(int value) {
-        this->value = value;
-        previous = nullptr;
-        next = nullptr;
-    }
-
-    Node(int value, Node *next, Node *previous) {
-        this->value = value;
-        this->next = next;
-        this->previous = previous;
+    Node(int color, int number) {
+        this->color = color;
+        this->number = number;
     }
 };
 
-Node* arr[20001];
-int k = 0;
+struct List
+{
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int count = 0;
 
-class DoublyLinkedList {
-public:
-    Node *head;
-    Node *tail;
+    Node* push_after(Node* node, Node* after) {
+        if (after == tail)
+            return push_back(node);
 
-    int elementsCount;
+        node->next = after->next;
+        node->previous = after;
+        after->next = node;
+        node->next->previous = node;
+        
+        ++count;
 
-    DoublyLinkedList() {
-        head = nullptr;
-        tail = nullptr;
-
-        elementsCount = 0;
+        return node;
     }
 
-    void push_back(int number) {
-        elementsCount++;
-
-        if (tail == nullptr) {
-            head = new Node(number, nullptr, nullptr);
-            tail = head;
-            return;
+    Node* push_back(Node* node) {
+        ++count;
+        
+        if (count == 1) {
+            head = tail = node;
+            return node;
         }
 
-        tail->next = new Node(number, nullptr, tail);
-        tail = tail->next;
+        node->previous = tail;
+        node->next = nullptr;
+        tail->next = node;
+        tail = node;
+
+        return node;
     }
 
-    void insertAfter(int idx, Node* newNode) {
-        if (arr[idx]->next == nullptr) {
-            arr[idx]->next = newNode;
-            newNode->previous = arr[idx];
-            newNode->next = nullptr;
-            tail = tail->next;
+    void remove(Node* left, Node* right) {
+        Node* current;
 
-            return;
+        if (!left) current = head;
+        else current = left->next;
+
+        while (current != right) {
+            Node* temp = current->next;
+            delete current;
+            current = temp;
+            
+            --count;
         }
 
-        newNode->next = arr[idx]->next;
-        arr[idx]->next->previous = newNode;
-        arr[idx]->next = newNode;
-        newNode->previous = arr[idx];
+        if(!left && !right) head = tail = nullptr;
+
+        if (!left && right) {
+            head = right;
+            head->previous = nullptr;
+        }
+
+        if (!right && left) {
+            tail = left;
+            tail->next = nullptr;
+        }
+
+        if (left && right) {
+            left->next = right;
+            right->previous = left;
+        }
     }
 
-    bool IsEmpty() const {
-        return (head == nullptr && tail == nullptr);
-    }
 
-    void print() const {
-        Node *current = head;
-        while (current != nullptr) {
-            cout << current->value << " ";
-            current = current->next;
-        }
+    int pop(Node* node, bool flag = false) {
+        if (count == 0 || node == nullptr) return 0;
 
-        cout << endl;
-    }
+        Node* left = node;
+        Node* right = node;
 
-    void Check(Node* added, int& deleted) {
-        Node* before = added;
-        Node* after = added;
+        long long leftCount = 0;
+        long long rightCount = 0;
 
-        int counter = 0;
-        while (before->previous != nullptr && before->previous->value == added->value) {
-            counter++;
-            before = before->previous;
-        }
-        while (after->next != nullptr && after->next->value == added->value) {
-            counter++;
-            after = after->next;
-        }
+        bool tailContained = false;
+        bool headContained = false;
 
-        if (counter >= 2) {
-            deleted += (counter + 1);
+        while (left->color == node->color) {
+            ++leftCount;
 
-            Node* copy = after->next;
-            while (before != copy) {
-                Node* tmp = before->next;
-
-                if (before->previous != nullptr)
-                    before->previous->next = before->next;
-                if (before->next != nullptr)
-                    before->next->previous = before->previous;
-                if (before->previous == nullptr)
-                    head = head->next;
-                if (before->next == nullptr)
-                    tail = tail->previous;
-
-                delete before;
-                before = tmp;
+            if (left == head) {
+                headContained = true;
+                break;
             }
 
-            if (before != nullptr && before->previous!=nullptr && before->value == before->previous->value)
-                Check(before, deleted);
+            left = left->previous;
+        }
+
+        while (right->color == node->color) {
+            ++rightCount;
+
+            if (right == tail) {
+                tailContained = true;
+                break;
+            }
+
+            right = right->next;
+        }
+
+        --rightCount;
+
+        if (((leftCount > 0 && rightCount > 0) || !flag) && leftCount + rightCount >= 3) {
+            remove(headContained ? nullptr : left, tailContained ? nullptr : right);
+            return leftCount + rightCount + (headContained || tailContained ? 0 : pop(left, true));
+        }
+
+        return 0;
+    }
+
+    void print() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->color << " ";
+            current = current->next;
         }
     }
 };
+
+Node* originalBalls[100000]{};
+Node* shootBalls[100000]{};
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     int n;
     cin >> n;
-
-    for (int i = 0; i < 200001; i++)
-        arr[i] = nullptr;
-
-    DoublyLinkedList list;
-    vector<int> res;
-
+    
+    List balls;
+    int color;
+    
     for (int i = 0; i < n; i++) {
-        int color;
         cin >> color;
-        list.push_back(color);
+        Node* node = new Node(color, i);
+        originalBalls[i] = balls.push_back(node);
     }
 
     int q;
     cin >> q;
-
+    
+    Node* current;
+    int ball;
+    
     for (int i = 0; i < q; i++) {
-        int p, c;
-        cin >> p >> c;
+        cin >> ball >> color;
 
-        int deleted = 0;
+        if (balls.count == 0) {
+            cout << "Game Over\n";
+            continue;
+        }
 
-        if (!list.IsEmpty()) {
-            arr[k] = new Node(c);
-            list.insertAfter(p, arr[k]);
-            list.Check(arr[k], deleted);
-            res.push_back(deleted);
-            deleted = 0;
-            k++;
-        }
-        else {
-            res.push_back(-1);
-        }
+        if (ball < n)
+            current = originalBalls[ball];
+        else
+            current = shootBalls[ball % n];
+
+        Node* node = new Node(color, n + 1);
+        shootBalls[i] = balls.push_after(node, current);
+
+        cout << balls.pop(shootBalls[i]) << "\n";
     }
 
-    for (int i = 0; i < res.size(); i++) {
-        if (res[i] != -1)
-            cout << res[i] << "\n";
-        else cout << "Game Over\n";
-    }
-
-    if (!list.IsEmpty())
-        list.print();
-    else cout << "-1" << '\n';
+    if (balls.count == 0)
+        cout << "-1\n";
+    else
+        balls.print();
 
     return 0;
 }
